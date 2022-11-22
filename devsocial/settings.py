09563088d7342efd593b19a830e25ed26a2f3bed
jnjerin <njerijoan24@gmail.com>
2022-11-22 16:52:49 +0300

    added styling for diffent pages and components

--- devsocial/settings.py
+++ devsocial/settings.py
@@ -119,11 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+# STATIC_ROOT =
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
